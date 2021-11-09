782028af39f9e2e926b2a04af2ab99eae6c363e3
Salomon Dion <salomon.dion@futurafric.com>
2021-11-09 08:13:59 +0000

    style: :lipstick: fix user form layout

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,10 +1,6 @@
 <x-app-layout>
     <x-section-header title="Rôles">
-        <x-slot name="actions">
-            <a href="{{ route('users.create') }}" class="btn btn-secondary">
-                Nouveau
-            </a>
-        </x-slot>
+
     </x-section-header>
 
     <livewire:roles.roles-table />

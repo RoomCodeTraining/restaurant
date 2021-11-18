f047c8d776021298d33887f014b5e19cc36efc67
Roomdada <dsieroger@gmail.com>
2021-11-18 20:11:57 +0000

    :tada: fix api auth & add menus endpoints

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    function login(Request $request)
+    {
+
+        
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['E-mail ou mot de passe incorrect']
+            ], 404);
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    
+}

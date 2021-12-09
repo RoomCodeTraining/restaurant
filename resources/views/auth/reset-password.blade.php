0754ab8a0c395311e33048853d870baeadfaa80c
Salomon Dion <salomon.dion@futurafric.com>
2021-12-09 06:15:53 +0000

    fix: :bug: corrige le formulaire de reinitialisation de mot de passe

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,9 +1,9 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <div class="flex font-bold justify-center mb-6 mt-2">
+                <img class="h-auto w-1/3" src="{{ asset('images/logo-ciprel-vf.png') }}">
+            </div>
         </x-slot>
 
         <!-- Validation Errors -->
@@ -15,33 +15,43 @@
             <!-- Password Reset Token -->
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+            <div class="w-full text-center px-2">
+                <!-- Email Address -->
+                <div class="w-full">
+                    <div class="flex items-center">
+                        <i class="ml-3 fill-current text-gray-400 text-xs z-10 fas fa-envelope-open-text"></i>
+                        <input type="email" id="email" placeholder="Entrez votre email" name="email"
+                            :value="old('email', $request->email)"
+                            class="-mx-6 px-8 w-full rounded py-2 bg-opacity-25 text-gray-600 focus:outline-none bg-gray-100"
+                            required autofocus />
+                    </div>
+                </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
+                <!-- Password -->
+                <div class="w-full mt-4">
+                    <div class="flex items-center">
+                        <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
+                        <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe"
+                            class="-mx-6 px-8 w-full rounded py-2 bg-opacity-25 text-gray-600 focus:outline-none bg-gray-100"
+                            required autocomplete="current-password" />
+                    </div>
+                </div>
+                <!-- Confirm Password -->
+                <div class="w-full mt-4">
+                    <div class="flex items-center">
+                        <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
+                        <input type="password" id="password_confirmation" name="password_confirmation"
+                            placeholder="Entrez à nouveau votre mot de passe"
+                            class="-mx-6 px-8 w-full rounded py-2 bg-opacity-25 text-gray-600 focus:outline-none bg-gray-100"
+                            required autocomplete="current-password" />
+                    </div>
+                </div>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
+                <button type="submit" class="w-full py-3 font-medium bg-primary-900 text-gray-100  focus:outline-none">
+                    Réinitialiser le mot de passe
+                </button>
             </div>
         </form>
     </x-auth-card>

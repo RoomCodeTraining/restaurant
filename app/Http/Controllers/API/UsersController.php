bd48ee72940af6a4293ef2d8a01d5ecb7d71a776
Salomon Dion <salomon.dion@futurafric.com>
2021-12-13 13:54:39 +0000

    feat: :sparkles: ajout endpoints api utilisateurs

---
+++ app/Http/Controllers/API/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::active()->get();
+
+        return UserResource::collection($users);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+}

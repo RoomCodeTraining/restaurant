80e2c7fa78afe577a6a366c4747bcf077422b6e9
Roomdada <dsieroger@gmail.com>
2022-01-13 10:03:06 +0000

    message d'erreur quant un utilisateur existe deja lors de la creation depuis un fichier csv

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -26,27 +26,34 @@ class UsersImport implements ToModel, WithHeadingRow
   
         DB::beginTransaction();
        //$employee_status_id = \App\Models\EmployeeStatus::where('name', $row['categorie'])->first()->id;
-       if (! User::whereIdentifier($row['matricule'])->exists()) {
-           $user = User::create([
-            'identifier' => $row['matricule'],
-            'username' => explode('@', $row['email'])[0],
-            'email' => $row['email'],
-            'last_name' => $row['nom'],
-            'first_name' => $row['prenoms'],
-            'contact' => $row['contact'],
-            'current_role_id' => Role::getRole($row['profil']) ?? Role::USER,
-            'employee_status_id' => 1,
-            'department_id' => \App\Models\Department::where('name', $row['departement'])->first()->id,
-            'organization_id' => \App\Models\Organization::where('name', $row['societe'])->first()->id,
-            'user_type_id' => \App\Models\UserType::where('name', $row['type_de_collaborateur'])->first()->id,
-            'email_verified_at' => now(),
-        ]);
+        $users_exist = User::whereIdentifier($row['matricule'])->exists() ? true : false;
+        
+        if ($users_exist) {
+            session()->flash('error', 'Il existe des utilisatuers du fichier qui existe déjà!');
+        }
+
+        if (! User::whereIdentifier($row['matricule'])->exists()) {
+            $user = User::create([
+                'identifier' => $row['matricule'],
+                'username' => explode('@', $row['email'])[0],
+                'email' => $row['email'],
+                'last_name' => $row['nom'],
+                'first_name' => $row['prenoms'],
+                'contact' => $row['contact'],
+                'current_role_id' => Role::getRole($row['profil']) ?? Role::USER,
+                'employee_status_id' => 1,
+                'department_id' => \App\Models\Department::where('name', $row['departement'])->first()->id,
+                'organization_id' => \App\Models\Organization::where('name', $row['societe'])->first()->id,
+                'user_type_id' => \App\Models\UserType::where('name', $row['type_de_collaborateur'])->first()->id,
+                'email_verified_at' => now(),
+            ]);
 
            $user->syncRoles(Role::getRole($row['profil']) ?? [Role::USER]);
 
            DB::commit();
 
            UserCreated::dispatch($user);
+
        }
 
         //$user->sendWelcomeNotification(now()->addWeek());

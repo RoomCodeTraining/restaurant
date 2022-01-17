ca14db47f73dff2b40f774df099c42964a68e102
Roomdada <dsieroger@gmail.com>
2022-01-17 15:29:38 +0000

    fix

--- resources/views/livewire/dashboards/admin-dashboard.blade.php
+++ resources/views/livewire/dashboards/admin-dashboard.blade.php
@@ -4,10 +4,10 @@
         @if (auth()->user()->current_access_card_id)
             <x-statistic label="Commande du jour" value="{{ $today_order ? $today_order->dish->name : 'Aucune' }}"
                 icon="plat" />
-            <x-statistic label="quota dejeuner" value="{{ auth()->user()->currentAccessCard->quota_breakfast }}"
-                icon="card" />
-            <x-statistic label="quota petit dejeuner" value="{{ auth()->user()->currentAccessCard->quota_lunch }}"
-                icon="card" />
+                <x-statistic label="quota dejeuner" value="{{ auth()->user()->currentAccessCard->quota_lunch }}"
+                    icon="card" />
+                <x-statistic label="quota petit dejeuner" value="{{ auth()->user()->currentAccessCard->quota_breakfast }}"
+                    icon="card" />
         @endif
         <x-statistic label="Utilisateurs" value="{{ $users_count }}" icon="users" />
         <x-statistic label="Utilisateurs actifs" value="{{ $active_users_count }}" icon="actifuser" />

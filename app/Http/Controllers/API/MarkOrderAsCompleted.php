ec733420840eecd3684f2e54992ad98d4c2b1324
Roomdada <dsieroger@gmail.com>
2022-01-27 18:01:26 +0000

    message erreur lors de la validation du petit dejeuner

--- app/Http/Controllers/API/MarkOrderAsCompleted.php
+++ app/Http/Controllers/API/MarkOrderAsCompleted.php
@@ -89,7 +89,7 @@ class MarkOrderAsCompleted extends Controller
          */
         if (! $order) {
             return response()->json([
-                "message" => "Vous n'avez pas de commande pour ce jour.",
+                "message" => "Vous ne pouvez pas effectuer cette action. Veuillez contacter un administrateur.",
                 "success" => false,
                 "user" => $accessCard->user
             ], Response::HTTP_NOT_FOUND);

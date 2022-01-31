0fd4d03c77eb6bf9a003357dc8a82260d9155e84
Roomdada <dsieroger@gmail.com>
2022-01-31 16:41:59 +0000

    fix

--- resources/views/livewire/suggestions/table-actions.blade.php
+++ resources/views/livewire/suggestions/table-actions.blade.php
@@ -1,14 +1,15 @@
-<div class="flex items-center">
-
-      <div x-data="{ tooltip: 'Modifier' }">
-          <a href="{{ route('suggestions-box.edit', $suggestion) }}" class="mr-2" x-tooltip="tooltip">
-              <x-icon name="pencil" class="h4 w-4 text-indigo-700" />
-          </a>
-      </div>
-         <div x-data="{ tooltip: 'Supprimer' }">
-          <button wire:click="confirmSuggestionDeletion({{ $suggestion->id }})" wire:loading.attr="disabled"
-              class="mr-2" x-tooltip="tooltip">
-              <x-icon name="trash" class="h4 w-4 text-red-700" />
-          </button>
-      </div>
-</div>
+@if(auth()->user()->can('manage', \App\Models\SuggestionBox::class))
+  <div class="flex items-center">
+        <div x-data="{ tooltip: 'Modifier' }">
+            <a href="{{ route('suggestions-box.edit', $suggestion) }}" class="mr-2" x-tooltip="tooltip">
+                <x-icon name="pencil" class="h4 w-4 text-indigo-700" />
+            </a>
+        </div>
+          <div x-data="{ tooltip: 'Supprimer' }">
+            <button wire:click="confirmSuggestionDeletion({{ $suggestion->id }})" wire:loading.attr="disabled"
+                class="mr-2" x-tooltip="tooltip">
+                <x-icon name="trash" class="h4 w-4 text-red-700" />
+            </button>
+        </div>
+  </div>
+@endif

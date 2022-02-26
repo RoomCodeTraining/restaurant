020ce10990254086cd3c54e814cd5fb3cdbb5d37
Roomdada <dsieroger@gmail.com>
2022-02-26 12:36:03 +0000

    Decrementation des cartes qui ont un quota

--- app/Console/Commands/ChargeUsers.php
+++ app/Console/Commands/ChargeUsers.php
@@ -9,37 +9,39 @@ use Illuminate\Support\Facades\DB;
 
 class ChargeUsers extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'charge:users';
- 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Réduction du quota de déjeuner des utilisateurs ayant commandé un plat';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'charge:users';
 
-    /**
-     * Débiter le quota lunch des utilisateurs qui ont une commande pour le jour en cours.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        Order::with('user.accessCard')->today()->whereState('state', Confirmed::class)->each(function (Order $order) {
-            DB::transaction(function () use ($order) {
-                $order->user->accessCard->decrement('quota_lunch');
-                $order->update([
-                    'payment_method_id' => $order->user->accessCard->payment_method_id,
-                    'access_card_id' => $order->user->accessCard->id,
-                ]);
-            });
-        });
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Réduction du quota de déjeuner des utilisateurs ayant commandé un plat';
 
-        return Command::SUCCESS;
-    }
+  /**
+   * Débiter le quota lunch des utilisateurs qui ont une commande pour le jour en cours.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    Order::with('user.accessCard')->today()->whereState('state', Confirmed::class)->each(function (Order $order) {
+      DB::transaction(function () use ($order) {
+        if ($order->user->accessCard->quota_lunch > 0) {
+          $order->user->accessCard->decrement('quota_lunch');
+          $order->update([
+            'payment_method_id' => $order->user->accessCard->payment_method_id,
+            'access_card_id' => $order->user->accessCard->id,
+          ]);
+        }
+      });
+    });
+
+    return Command::SUCCESS;
+  }
 }

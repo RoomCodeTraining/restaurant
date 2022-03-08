02bd121ded73d31340aad851ea523fde2845840b
Roomdada <dsieroger@gmail.com>
2022-03-08 07:40:45 +0000

    fix column exceptional

--- database/migrations/2022_03_08_073656_add_is_exceptional_to_orders.php
+++ database/migrations/2022_03_08_073656_add_is_exceptional_to_orders.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterOrdersTable extends Migration
+class AddIsExceptionalToOrders extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,7 @@ class AlterOrdersTable extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->dropForeign('orders_dish_id_foreign');
-            $table->dropForeign('orders_menu_id_foreign');
-            $table->foreignId('dish_id')->nullable()->change();
-            $table->foreignId('menu_id')->nullable()->change();
-            $table->foreignId('payment_method_id')->nullable();
-            $table->enum('type', ['lunch', 'breakfast'])->default('lunch');
-            $table->index(['dish_id', 'menu_id', 'type']);
+          $table->boolean('is_exceptional')->default(false);
         });
     }
 

fd1cdd2f6742679bbf64b13065b690f1d88e8aaa
Roomdada <dsieroger@gmail.com>
2022-03-11 12:48:14 +0000

    style::lipstick: customisation des logs de rechargement

--- app/Http/Livewire/AccessCards/TopUpForm.php
+++ app/Http/Livewire/AccessCards/TopUpForm.php
@@ -11,86 +11,120 @@ use Illuminate\Validation\ValidationException;
 
 class TopUpForm extends Component
 {
-    public $user;
+  public $user;
 
-    public $state = [
-        'quota_breakfast' => 0,
-        'quota_lunch' => 0,
-        'payment_method_id' => null,
-    ];
+  public $state = [
+    'quota_breakfast' => 0,
+    'quota_lunch' => 0,
+    'payment_method_id' => null,
+  ];
 
-    public function mount(User $user)
-    {
-        $this->user = $user->load('userType.paymentMethod', 'accessCard.paymentMethod');
+  public function mount(User $user)
+  {
+    $this->user = $user->load('userType.paymentMethod', 'accessCard.paymentMethod');
 
-        if ($this->user->accessCard) {
-            $this->state = [
-                'quota_breakfast' => $this->user->accessCard->quota_breakfast,
-                'quota_lunch' => $this->user->accessCard->quota_lunch,
-            ];
-        }
-        $paymentMethod = optional($this->user->accessCard)->paymentMethod->name ?? $this->user->userType->paymentMethod->name;
-        $this->state['payment_method_id'] = PaymentMethod::firstWhere('name', $paymentMethod)->id;
+    if ($this->user->accessCard) {
+      $this->state = [
+        'quota_breakfast' => $this->user->accessCard->quota_breakfast,
+        'quota_lunch' => $this->user->accessCard->quota_lunch,
+      ];
+    }
+    $paymentMethod = optional($this->user->accessCard)->paymentMethod->name ?? $this->user->userType->paymentMethod->name;
+    $this->state['payment_method_id'] = PaymentMethod::firstWhere('name', $paymentMethod)->id;
+  }
+
+  public function topUp()
+  {
+    //$this->resetErrorBag();
+
+    $this->validate([
+      'state.quota_breakfast' => ['required', 'integer', 'min:0', 'max:25'],
+      'state.quota_lunch' => ['required', 'integer', 'min:0', 'max:25'],
+      'state.payment_method_id' => ['required', Rule::exists('payment_methods', 'id')],
+    ]);
+
+
+    if (!$this->user->accessCard) {
+      throw ValidationException::withMessages([
+        'state.payment_method_id' => ["Cet utilisateur ne dispose pas de carte RFID associée à son compte."],
+      ]);
     }
 
-    public function topUp()
-    {
-        //$this->resetErrorBag();
+    $this->updateCountOfReload($this->user->accessCard);
+    $updated = $this->allIsUpdated($this->user->accessCard);
+ 
+    $this->user->accessCard->quota_breakfast =  (int) $this->state['quota_breakfast'];
+    $this->user->accessCard->quota_lunch = (int) $this->state['quota_lunch'];
+    $this->user->accessCard->payment_method_id = $this->state['payment_method_id'];
+    $this->user->accessCard->save();
 
-        $this->validate([
-            'state.quota_breakfast' => ['required', 'integer', 'min:0', 'max:25'],
-            'state.quota_lunch' => ['required', 'integer', 'min:0', 'max:25'],
-            'state.payment_method_id' => ['required', Rule::exists('payment_methods', 'id')],
-        ]);
+    $logMessage = $this->logMessage($this->user->accessCard, $updated);
+
+    activity()
+      ->causedBy(Auth()->user())
+      ->performedOn($this->user->accessCard)
+      ->event($logMessage)
+      ->log('Rechargement de carte RFID');
+
+    $selectedPaymentMethod = $this->state['payment_method_id'];
+
+    $this->reset(['state']);
+
+    $this->state['payment_method_id'] = $selectedPaymentMethod;
+    session()->flash('success', "Le rechargement a été effectué avec succès.");
+
+    return redirect()->route('users.show', $this->user);
+  }
 
 
-        if (! $this->user->accessCard) {
-            throw ValidationException::withMessages([
-                'state.payment_method_id' => ["Cet utilisateur ne dispose pas de carte RFID associée à son compte."],
-            ]);
-        }
-        
-        $this->updateCountOfReload($this->user->accessCard);
+  public function updateCountOfReload(AccessCard $accessCard)
+  {
 
-        $this->user->accessCard->quota_breakfast =  (int) $this->state['quota_breakfast'];
-        $this->user->accessCard->quota_lunch = (int) $this->state['quota_lunch'];
-        $this->user->accessCard->payment_method_id = $this->state['payment_method_id'];
-        $this->user->accessCard->save();
-
-        activity()
-        ->causedBy(Auth()->user())
-        ->performedOn($this->user->accessCard)
-        ->event("La carte de l'utilisateur {$this->user->full_name} a été rechargée par ".auth()->user()->full_name)
-        ->log('Rechargement de carte RFID');
-
-        $selectedPaymentMethod = $this->state['payment_method_id'];
-
-        $this->reset(['state']);
-
-        $this->state['payment_method_id'] = $selectedPaymentMethod;
-        session()->flash('success', "Le rechargement a été effectué avec succès.");
-
-        return redirect()->route('users.show', $this->user);
+    if ($accessCard->quota_lunch != $this->state['quota_lunch']) {
+      $accessCard->createReloadHistory('lunch');
+    }
+    if ($accessCard->quota_breakfast != $this->state['quota_breakfast']) {
+      $accessCard->createReloadHistory('breakfast');
     }
 
+    return $accessCard;
+  }
 
-    public function updateCountOfReload(AccessCard $accessCard)
-    {
-      
-       if($accessCard->quota_lunch != $this->state['quota_lunch']){
-         $accessCard->createReloadHistory('lunch');
-       }
-        if($accessCard->quota_breakfast != $this->state['quota_breakfast']){
-          $accessCard->createReloadHistory('breakfast');
-        }
 
-        return $accessCard;
+  // Message du log lors du rechargement de carte RFID
+
+  public function allIsUpdated(AccessCard $accessCard): string
+  {
+    if ($accessCard->quota_lunch != $this->state['quota_lunch'] && $accessCard->quota_breakfast != $this->state['quota_breakfast']) {
+      return 'all';
     }
 
-    public function render()
-    {
-        return view('livewire.access-cards.top-up-form', [
-            'paymentMethods' => PaymentMethod::pluck('name', 'id')
-        ]);
+    if ($accessCard->quota_lunch != $this->state['quota_lunch']) {
+      return 'lunch';
     }
+
+    return 'breakfast';
+  }
+
+
+
+  public function logMessage(AccessCard $card, string $asUpdated): string
+  {
+    if ($asUpdated == 'all') {
+      return "Les quotas petit déjeuner et déjeuner de " . $card->user->full_name . " ont été rechargeés par " . auth()->user()->full_name . " le nouveau quota petit déjeuner est de " . $card->quota_breakfast . " et le nouveau quota déjeuner est de " . $card->quota_lunch;
+    }
+
+    if ($asUpdated == 'lunch') {
+      return "Le quota déjeuner de " . $card->user->full_name . " a été rechargeé par " . auth()->user()->full_name . " et le nouveau quota déjeuner est de " . $card->quota_lunch;
+    }
+
+    return "Le quota petit déjeuner de " . $card->user->full_name . " a été rechargeé par " . auth()->user()->full_name . " et le nouveau quota petit déjeuner est de " . $card->quota_breakfast;
+  }
+
+  public function render()
+  {
+    return view('livewire.access-cards.top-up-form', [
+      'paymentMethods' => PaymentMethod::pluck('name', 'id')
+    ]);
+  }
 }

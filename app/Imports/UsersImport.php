a05a8b42f60a6d174052130f7098d7305f6e7036
Roomdada <dsieroger@gmail.com>
2022-03-14 14:57:04 +0000

    refactor::recycle: Revue de l'import des utilisateurs

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -15,66 +15,70 @@ use Maatwebsite\Excel\Concerns\WithValidation;
 
 class UsersImport implements ToModel, WithHeadingRow, WithValidation
 {
-    use Importable;
+  use Importable;
 
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
+  /**
+   * @param array $row
+   *
+   * @return \Illuminate\Database\Eloquent\Model|null
+   */
+  public function model(array $row)
+  {
 
-            //Informatique
-        
-  
-           DB::beginTransaction();       
-                $data = $this->getUserhasBeingCreatedData($row);
-                $user = User::create($data);
-                $user->syncRoles(Role::getRole(strtolower($row['profil'])) ?? [Role::USER]);
-           DB::commit();
-
-           UserCreated::dispatch($user);
-           session()->flash('success', 'Les utilisateurs ont été importés!');
-
-   
-
-        //$user->sendWelcomeNotification(now()->addWeek());
-    }
+    //Informatique
 
 
-    public function rules(): array
-    {
-        return [
-            'matricule' => 'required',
-            'prenoms' => 'nullable|string',
-            'nom' => 'required|string',
-            'contact' => 'required',
-            'societe' => 'required|string',
-            'email' => ['required', 'email', 'unique:users'],
-            'categorie' =>  'required|string',
-            'departement' => ['required','string', Rule::exists('departments', 'name')],
-            'profil' => 'required|string',
-            'type' => 'required'
-        ];
-    }
+    DB::beginTransaction();
+
+      $data = $this->getUserhasBeingCreatedData($row);
+      $user = User::updateOrCreate([
+        'identifier' => $data['identifier'],
+      ], $data);
+      $user->syncRoles(Role::getRole(strtolower($row['profil'])) ?? [Role::USER]);
+
+    DB::commit();
+
+    UserCreated::dispatch($user);
+    session()->flash('success', 'Les utilisateurs ont été importés!');
 
 
-    public function getUserhasBeingCreatedData($model) : Array
-    {
-        return[
-            'employee_status_id' => \App\Models\EmployeeStatus::whereName(trim(ucfirst($model['categorie'])))->first()->id,
-            'organization_id' =>\App\Models\Organization::whereName(trim(ucfirst($model['societe'])))->first()->id,
-            'department_id' => \App\Models\Department::whereName(trim(ucfirst($model['departement'])))->first()->id,
-            'user_type_id' => \App\Models\UserType::whereName(trim(ucfirst($model['type'])))->first()->id,
-            'identifier' => $model['matricule'],
-            'username' => explode('@', $model['email'])[0],
-            'email' => $model['email'],
-            'last_name' => $model['nom'],
-            'first_name' => $model['prenoms'],
-            'contact' => $model['contact'],
-            'current_role_id' => Role::getRole(strtolower($model['profil'])) ?? Role::USER,
-            'email_verified_at' => now(),
-        ];       
-    }
+
+    //$user->sendWelcomeNotification(now()->addWeek());
+  }
+
+
+  public function rules(): array
+  {
+    return [
+      'matricule' => 'required',
+      'prenoms' => 'nullable|string',
+      'nom' => 'required|string',
+      'contact' => 'required',
+      'societe' => 'required|string',
+      'email' => ['required', 'email'],
+      'categorie' =>  'required|string',
+      'departement' => ['required', 'string', Rule::exists('departments', 'name')],
+      'profil' => 'required|string',
+      'type' => 'required'
+    ];
+  }
+
+
+  public function getUserhasBeingCreatedData($model): array
+  {
+    return [
+      'employee_status_id' => \App\Models\EmployeeStatus::whereName(trim(ucfirst($model['categorie'])))->first()->id,
+      'organization_id' => \App\Models\Organization::whereName(trim(ucfirst($model['societe'])))->first()->id,
+      'department_id' => \App\Models\Department::whereName(trim(ucfirst($model['departement'])))->first()->id,
+      'user_type_id' => \App\Models\UserType::whereName(trim(ucfirst($model['type'])))->first()->id,
+      'identifier' => $model['matricule'],
+      'username' => explode('@', $model['email'])[0],
+      'email' => $model['email'],
+      'last_name' => $model['nom'],
+      'first_name' => $model['prenoms'],
+      'contact' => $model['contact'],
+      'current_role_id' => Role::getRole(strtolower($model['profil'])) ?? Role::USER,
+      'email_verified_at' => now(),
+    ];
+  }
 }

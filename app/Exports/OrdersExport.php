e0b780e8e66266e6d4c489599359f96ced2292dd
Roomdada <dsieroger@gmail.com>
2022-10-05 12:15:17 +0000

    fix

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -20,128 +20,131 @@ use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class OrdersExport implements FromCollection, WithTitle, WithMapping, WithHeadings, WithStyles, ShouldAutoSize
 {
-  use Exportable;
+    use Exportable;
 
-  protected $period;
-  protected $state;
+    protected $period;
+    protected $state;
 
-  public function __construct(?string $period, ?string $state)
-  {
-    $this->period = $period;
-    $this->state = $state;
-  }
+    public function __construct(?string $period, ?string $state)
+    {
+        $this->period = $period;
+        $this->state = $state;  
+    }
 
-  public function collection()
-  {
+    public function collection()
+    {
+     
+        return Order::query()
+            ->join('users', 'orders.user_id', 'users.id')
+            ->join('user_types', 'users.user_type_id', 'user_types.id')
+            ->join('menus', 'orders.menu_id', 'menus.id')
+            ->with('menu', 'user.role', 'user.department', 'user.employeeStatus', 'user.userType', 'user.accessCard')
+            ->unless($this->state, fn ($query) => $query->whereState('state', [Confirmed::class, Completed::class]))
+            ->when($this->state, fn ($query) => $query->whereState('state', $this->state))
+            ->whereBetween('menus.served_at', DateTimeHelper::inThePeriod($this->period))
+            ->get()->groupBy('user_id')
+            ->map(fn ($row) => $row->groupBy(fn ($item) =>  $item->menu->served_at->format('Y-m-d')))
+            ->flatten(1);
+    }
 
-    return Order::query()
-      ->join('users', 'orders.user_id', 'users.id')
-      ->join('user_types', 'users.user_type_id', 'user_types.id')
-      ->join('menus', 'orders.menu_id', 'menus.id')
-      ->with('menu', 'user.role', 'user.department', 'user.employeeStatus', 'user.userType', 'user.accessCard')
-      ->unless($this->state, fn ($query) => $query->whereState('state', [Confirmed::class, Completed::class]))
-      ->when($this->state, fn ($query) => $query->whereState('state', $this->state))
-      ->whereBetween('menus.served_at', DateTimeHelper::inThePeriod($this->period))
-      ->get()->groupBy('user_id')
-      ->map(fn ($row) => $row->groupBy(fn ($item) =>  $item->menu->served_at->format('Y-m-d')))
-      ->flatten(1);
-  }
+    public function title(): string
+    {
+        [$start, $end] = DateTimeHelper::inThePeriod($this->period);
+     
+        switch ($this->state) {
+            case 'confirmed':
+                $state =  'Commande non consommé';
+                break;
+            case 'completed':
+                $state = 'Commande consommé';
+                break;
+            default:
+               $state = "Toutes les commandes";
+                break;
+        }
+        
+     
+        return " $state du " . $start->format('d/m/y') . ' au ' . $end->format('d/m/y');
+    }
 
-  public function title(): string
-  {
-    [$start, $end] = DateTimeHelper::inThePeriod($this->period);
+    public function headings(): array
+    {
+        return [
+            "Matricule",
+            "Nom",
+            "Prénom",
+            "Email",
+            "Contact",
+            "Rôle",
+            "Société",
+            "Département",
+            "Type de collaborateur",
+            "Catégorie professionnelle",
+            "Date",
+            "Méthode de paiement",
+            "Type du plat",
+            "Statut du plat",
+            "Contribution collaborateur",
+            "Subvention ciprel",
+        ];
+    }
 
-    switch ($this->state) {
-      case 'confirmed':
-        $state =  'Commande non consommé';
-        break;
-      case 'completed':
-        $state = 'Commande consommé';
-        break;
-      default:
-        $state = "Toutes les commandes";
-        break;
+    public function map($row): array
+    {
+        $order = $row->count() > 1 ? $row->where('type', 'lunch')->first() : $row->first();
+        $date = $order->type == 'lunch' ? $order->menu->served_at : $order->created_at;
+
+        // Recuperation de la facturation
+        if($order->user){
+          $userBill = BillingHelper::getUserBill($order->user, $row);
+          $contribution =  $userBill['contribution'];
+          $subvention = $userBill['subvention'];
+        }else{
+          $contribution = '(N/A)';
+          $subvention = '(N/A)';  
+        }
+        
+
+        return [
+            $order->user?->identifier,
+            $order->user?->last_name,
+            $order->user?->first_name,
+            $order->user?->email,
+            $order->user?->contact,
+            $order->user?->role->name,
+            $order->user?->organization->name,
+            $order->user?->department->name,
+            $order->user?->userType->name,
+            $order->user?->employeeStatus->name,
+            $date->format('d/m/Y'),
+            $order->user?->accessCard->paymentMethod->name,
+            "Déjeuner",
+            $order->state::description(),
+            (string) $contribution,
+            (string) $subvention,
+        ];
     }
 
 
-    return " $state du " . $start->format('d/m/y') . ' au ' . $end->format('d/m/y');
-  }
 
-  public function headings(): array
-  {
-    return [
-      "Matricule",
-      "Nom",
-      "Prénom",
-      "Email",
-      "Contact",
-      "Rôle",
-      "Société",
-      "Département",
-      "Type de collaborateur",
-      "Catégorie professionnelle",
-      "Date",
-      "Méthode de paiement",
-      "Type du plat",
-      "Statut du plat",
-      "Contribution collaborateur",
-      "Subvention ciprel",
-    ];
-  }
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->setAutoFilter('A1:P' . $sheet->getHighestRow());
 
-  public function map($row): array
-  {
-    $order = $row->count() > 1 ? $row->where('type', 'lunch')->first() : $row->first();
-    $date = $order->type == 'lunch' ? $order->menu->served_at : $order->created_at;
+        $sheet->getStyle('A1:P1')->applyFromArray([
+            'font' => ['color' => ['rgb' => 'FFFFFF'], 'bold' => true, 'size' => 11],
+            'fill' => ['fillType' => Fill::FILL_SOLID, 'startColor' => ['rgb' => '538ED5']]
+        ]);
 
-    // Recuperation de la facturation
-    if ($order->user) {
-      $userBill = BillingHelper::getUserBill($order->user, $row);
-      $contribution =  $userBill['contribution'];
-      $subvention = $userBill['subvention'];
+        $sheet->getRowDimension(1)->setRowHeight(15);
 
-      return [
-        $order->user->identifier,
-        $order->user->last_name,
-        $order->user->first_name,
-        $order->user->email,
-        $order->user->contact,
-        $order->user->role->name,
-        $order->user->organization->name,
-        $order->user->department->name,
-        $order->user->userType->name,
-        $order->user->employeeStatus->name,
-        $date->format('d/m/Y'),
-        $order->user->accessCard->paymentMethod->name,
-        "Déjeuner",
-        $order->state::description(),
-        (string) $contribution,
-        (string) $subvention,
-      ];
+        $sheet->getStyle('A2:P' . $sheet->getHighestRow())->applyFromArray([
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['rgb' => '000000'],
+                ],
+            ],
+        ]);
     }
-    
-  }
-
-
-
-  public function styles(Worksheet $sheet)
-  {
-    $sheet->setAutoFilter('A1:P' . $sheet->getHighestRow());
-
-    $sheet->getStyle('A1:P1')->applyFromArray([
-      'font' => ['color' => ['rgb' => 'FFFFFF'], 'bold' => true, 'size' => 11],
-      'fill' => ['fillType' => Fill::FILL_SOLID, 'startColor' => ['rgb' => '538ED5']]
-    ]);
-
-    $sheet->getRowDimension(1)->setRowHeight(15);
-
-    $sheet->getStyle('A2:P' . $sheet->getHighestRow())->applyFromArray([
-      'borders' => [
-        'allBorders' => [
-          'borderStyle' => Border::BORDER_THIN,
-          'color' => ['rgb' => '000000'],
-        ],
-      ],
-    ]);
-  }
 }

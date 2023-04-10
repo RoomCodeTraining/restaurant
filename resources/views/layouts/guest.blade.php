89b4d1599ae13388290990f4fe0e5fcdbe2e6f9a
Roomdada <dsieroger@gmail.com>
2023-04-10 09:09:40 +0000

    fix::bug:

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,9 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css" />
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">

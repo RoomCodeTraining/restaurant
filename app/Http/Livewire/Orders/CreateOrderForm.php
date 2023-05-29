8143a5bccbc5576ad6510c7e80db79a8afc47c63
Roomdada <dsieroger@gmail.com>
2023-05-29 14:43:40 +0000

    Prise en compte de 2 plat pour les gendarmes

--- app/Http/Livewire/Orders/CreateOrderForm.php
+++ app/Http/Livewire/Orders/CreateOrderForm.php
@@ -54,6 +54,21 @@ class CreateOrderForm extends Component
         $this->resetErrorBag();
 
         $this->validate(['selectedDishes' => ['required', 'array']]);
+        if(auth()->user()->can_order_two_dishes){
+           foreach($this->selectedDishes as $menuId => $items){
+            $menu = \App\Models\Menu::whereId($menuId)->first();
+              foreach($items as $item){
+                \App\Models\Order::create([
+                    'dish_id' => $item['id'],
+                    'menu_id' => $menu->id,
+                    'user_id' => Auth::id()
+                  ]);
+              }
+           }
+
+           flasher('success', 'Votre commande a été enregistrée avec succès !');
+           return redirect()->route('orders.index');
+        }
 
         /**
          * S'assure que le quota de commande de l'utilisateur est suffisant.
@@ -169,7 +184,17 @@ class CreateOrderForm extends Component
      */
     public function addDish($menuId, $dishId)
     {
-        $this->selectedDishes[$menuId] = $this->menus->firstWhere('id', $menuId)->dishes->firstWhere('id', $dishId);
+
+        if(auth()->user()->can_order_two_dishes){
+            if(! empty($this->selectedDishes[$menuId])){
+                $this->selectedDishes[$menuId][1] = $this->menus->firstWhere('id', $menuId)->dishes->firstWhere('id', $dishId);
+            }else{
+                $this->selectedDishes[$menuId][0] = $this->menus->firstWhere('id', $menuId)->dishes->firstWhere('id', $dishId);
+            }
+        }else{
+          $this->selectedDishes[$menuId] = $this->menus->firstWhere('id', $menuId)->dishes->firstWhere('id', $dishId);
+        }
+
         $this->resetValidation();
     }
 

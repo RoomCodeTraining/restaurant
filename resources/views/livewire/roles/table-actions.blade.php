f433a57b15bf2dd6d1b2521377d7479c923d000a
roomdada <dsieroger@gmail.com>
2023-05-29 03:01:50 +0000

    fix

--- resources/views/livewire/roles/table-actions.blade.php
+++ resources/views/livewire/roles/table-actions.blade.php
@@ -1,10 +1,11 @@
 <div class="flex items-center">
-
+    @can('user.update')
         <div x-data="{ tooltip: 'Modifier' }">
             <a href="{{ route('roles.edit', $role) }}" class="mr-2" x-tooltip="tooltip">
                 <x-icon name="pencil" class="h4 w-4 text-accent-800" />
             </a>
         </div>
+    @endcan
     @if ($role->users_count === 0)
         <div x-data="{ tooltip: 'Supprimer' }">
             <button wire:click="confirmRoleDeletion({{ $role->id }})" wire:loading.attr="disabled"

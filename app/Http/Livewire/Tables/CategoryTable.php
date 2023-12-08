b01758c862390ead05d9b1aaf7aa52cfdcb6b8b3
roomdada <dsieroger@gmail.com>
2023-12-08 11:38:11 +0100

    wip

--- app/Http/Livewire/Tables/CategoryTable.php
+++ app/Http/Livewire/Tables/CategoryTable.php
@@ -21,34 +21,47 @@ class CategoryTable extends Component implements HasTable, HasForms
     public function table(Table $table): Table
     {
         return $table
-            ->query(\App\Models\EmployeeStatus::query()->withCount('users')->latest())
+            ->query(
+                \App\Models\EmployeeStatus::query()
+                    ->withCount('users')
+                    ->latest(),
+            )
             ->columns([
-                TextColumn::make('created_at')->label('DATE DE CRÉATION')->searchable()->dateTime('d/m/Y'),
-                TextColumn::make('name')->label('NOM')->searchable(),
+                TextColumn::make('created_at')
+                    ->label('DATE DE CRÉATION')
+                    ->searchable()
+                    ->dateTime('d/m/Y'),
+                TextColumn::make('name')
+                    ->label('NOM')
+                    ->searchable(),
                 TextColumn::make('users_count')->label('NBR D\'ÉMPLOYÉS'),
             ])
             ->actions([
-                    Action::make('Editer')
-                        ->url(fn (EmployeeStatus $record): string => route('employeeStatuses.edit', $record))
-                        ->color('info')
-                        ->label('')
-                        ->tooltip('Editer la catégorie professionnelle')
-                        ->icon('heroicon-o-pencil-square'),
+                Action::make('Editer')
+                    ->url(fn (EmployeeStatus $record): string => route('employeeStatuses.edit', $record))
+                    ->color('info')
+                    ->label('')
+                    ->tooltip('Editer la catégorie professionnelle')
+                    ->icon('heroicon-o-pencil-square'),
 
-                    Action::make('Supprimer')
-                        ->requiresConfirmation()
-                        ->icon('heroicon-o-trash')
-                        ->label('')
-                        ->tooltip('Supprimer la catégorie professionnelle')
-                        ->modalHeading('Supprimer la catégorie professionnelle')
-                        ->modalDescription('Etes-vous sûr de vouloir supprimer cette catégorie professionnelle ?')
-                        ->color('danger')
-                        ->hidden(fn (EmployeeStatus $record) => $record->users->count() > 0)
-                        ->action(function ($record) {
-                            $record->delete();
-                            ActivityHelper::createActivity($record, 'Suppression de la catégorie professionnelle ' . $record->name, 'Suppression de la catégorie professionnelle');
-                            Notification::make()->title('Suppression de la catégorie professionnelle')->success()->body('La catégorie professionnelle a été supprimée avec succès !')->send($record->user);
-                        })
+                Action::make('Supprimer')
+                    ->requiresConfirmation()
+                    ->icon('heroicon-o-trash')
+                    ->label('')
+                    ->tooltip('Supprimer la catégorie professionnelle')
+                    ->modalHeading('Supprimer la catégorie professionnelle')
+                    ->modalDescription('Etes-vous sûr de vouloir supprimer cette catégorie professionnelle ?')
+                    ->color('danger')
+                    ->hidden(fn (EmployeeStatus $record) => $record->users->count() > 0)
+                    ->action(function ($record) {
+                        $record->delete();
+                        ActivityHelper::createActivity($record, 'Suppression de la catégorie professionnelle ' . $record->name, 'Suppression de la catégorie professionnelle');
+                        Notification::make()
+                            ->title('Suppression de la catégorie professionnelle')
+                            ->success()
+                            ->body('La catégorie professionnelle a été supprimée avec succès !')
+                            ->send($record->user);
+                    }),
             ]);
     }
 
--- app/Http/Livewire/Tables/UserTable.php
+++ app/Http/Livewire/Tables/UserTable.php
@@ -2,30 +2,29 @@
 
 namespace App\Http\Livewire\Tables;
 
-use App\Models\User;
-use Livewire\Component;
 use App\Events\UserLocked;
-use Filament\Tables\Table;
-use App\Exports\UserExport;
-use Illuminate\Support\Str;
 use App\Events\UserUnlocked;
+use App\Exports\UserExport;
+use App\Models\User;
+use App\Notifications\PasswordResetNotification;
 use App\Support\ActivityHelper;
-use Filament\Tables\Actions\Action;
-use Illuminate\Support\Facades\Hash;
-use Maatwebsite\Excel\Facades\Excel;
+use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Notifications\Notification;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Contracts\HasTable;
-use Filament\Notifications\Notification;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Filters\SelectFilter;
-use pxlrbt\FilamentExcel\Exports\ExcelExport;
-use Filament\Forms\Concerns\InteractsWithForms;
-use App\Notifications\PasswordResetNotification;
 use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Maatwebsite\Excel\Facades\Excel;
 use pxlrbt\FilamentExcel\Actions\Tables\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class UserTable extends Component implements HasTable, HasForms
 {
@@ -71,7 +70,7 @@ class UserTable extends Component implements HasTable, HasForms
 
         return $table
             ->query(\App\Models\User::query()->latest())
-            ->paginated([10, 25, 50, 100, 'all'])
+            // ->paginated([10, 25, 50, 100, 'all'])
             ->columns([
                 TextColumn::make('identifier')
                     ->label('Matricule')
@@ -95,10 +94,6 @@ class UserTable extends Component implements HasTable, HasForms
                     ->formatStateUsing(function (User $row) {
                         return $row->isActive() ? 'Actif' : 'Inactif';
                     }),
-
-                TextColumn::make('accessCard.id')->label('Actions')->formatStateUsing(fn (User $user) => view('livewire.users.table-actions', ['user' => $user]))
-
-
             ])
             ->headerActions([
                 ExportAction::make()->exports([
@@ -116,108 +111,93 @@ class UserTable extends Component implements HasTable, HasForms
                     ->options([
                         '1' => 'Actif',
                         '0' => 'Inactif',
-
                     ])
-            ]);
-        // ->actions([
-        //     ViewAction::make()
-        //         ->label('')
-        //         ->icon('heroicon-o-eye')
-        //         ->color('success')
-        //         ->tooltip('Consulter')
-        //         ->url(function (User $user) {
-        //             return route('users.show', $user);
-        //         }),
-        //     EditAction::make()
-        //         ->label('')
-        //         ->icon('heroicon-o-pencil-square')
-        //         ->color('info')
-        //         ->tooltip('Modifier')
-        //         ->url(function (User $user) {
-        //             return route('users.edit', $user);
-        //         }),
-        //     DeleteAction::make()
-        //         ->label('')
-        //         ->icon('heroicon-o-lock-closed')
-        //         ->action(function (User $user) {
-        //             $user->is_active = false;
-        //             $user->save();
-        //             Notification::make()
-        //                 ->title('Utilisateur désactivé')
-        //                 ->body('L\'utilisateur a été désactivé avec succès.')
-        //                 ->success()
-        //                 ->send();
+            ])
+        ->actions([
+            ViewAction::make()
+                ->label('')
+                ->icon('heroicon-o-eye')
+                ->color('success')
+                ->tooltip('Consulter')
+                ->url(function (User $user) {
+                    return route('users.show', $user);
+                }),
+            EditAction::make()
+                ->label('')
+                ->icon('heroicon-o-pencil-square')
+                ->color('info')
+                ->tooltip('Modifier')
+                ->url(function (User $user) {
+                    return route('users.edit', $user);
+                }),
+            Action::make('lock')
+                ->label('')
+                ->icon('heroicon-o-lock-closed')
+                ->color('danger')
+                ->tooltip('Désactiver le compte')
+                ->modalHeading('Désactiver le compte')
+                ->modalDescription('Etes-vous sûr de vouloir désactiver ce compte ?')
+                ->action(function (User $user) {
+                    $this->lockUser($user);
+                    Notification::make()
+                        ->title('Utilisateur désactivé')
+                        ->body('L\'utilisateur a été désactivé avec succès.')
+                        ->success()
+                        ->send();
 
-        //             return redirect()->route('users.index');
-        //         })
-        //         ->tooltip('Desactiver')
-        //         ->hidden(function (User $user) {
-        //             return   !$user->isActive() || $user->id == auth()->user()->id;
-        //         })
-        //         ->requiresConfirmation(),
-        //     Action::make('reactivate')
-        //         ->label('')
-        //         ->icon('heroicon-o-lock-open')
-        //         ->tooltip('Activer')
-        //         ->hidden(function (User $user) {
-        //             return $user->isActive();
-        //         })
-        //         ->requiresConfirmation()
-        //         ->action(function (User $user) {
-        //             $user->is_active = true;
-        //             $user->save();
-        //             Notification::make()
-        //                 ->title('Utilisateur activé')
-        //                 ->body('L\'utilisateur a été activé avec succès.')
-        //                 ->success()
-        //                 ->send();
+                    return redirect()->route('users.index');
+                })
+                ->tooltip('Desactiver')
+                ->hidden(function (User $user) {
+                    return   ! $user->isActive() || $user->id == auth()->user()->id;
+                })
+                ->requiresConfirmation(),
+            Action::make('reactivate')
+                ->label('')
+                ->icon('heroicon-o-lock-open')
+                ->tooltip('Activer')
+                ->hidden(function (User $user) {
+                    return $user->isActive();
+                })
+                ->requiresConfirmation()
+                ->modalHeading('Activer le compte')
+                ->modalDescription('Etes-vous sûr de vouloir activer ce compte ?')
+                ->color('success')
+                ->action(function (User $user) {
+                    $this->unlockUser($user);
+                    Notification::make()
+                        ->title('Utilisateur activé')
+                        ->body('L\'utilisateur a été activé avec succès.')
+                        ->success()
+                        ->send();
 
-        //             return redirect()->route('users.index');
-        //         }),
-        //     DeleteAction::make()
-        //         ->label('')
-        //         ->icon('heroicon-o-trash')
-        //         ->action(function (User $user) {
-        //             $user->delete();
-        //             Notification::make()
-        //                 ->title('Utilisateur supprimé')
-        //                 ->body('L\'utilisateur a été supprimé avec succès.')
-        //                 ->success()
-        //                 ->send();
+                    return redirect()->route('users.index');
+                }),
+            Action::make('delete')
+                ->label('')
+                ->icon('heroicon-o-trash')
+                ->action(function (User $user) {
+                    $this->deleteUser($user);
 
-        //             return redirect()->route('users.index');
-        //         })
-        //         ->tooltip('Supprimer')
-        //         ->hidden(function (User $user) {
-        //             return $user->id == auth()->user()->id;
-        //         })
-        //         ->requiresConfirmation(),
-        // ]);
+                    Notification::make()
+                        ->title('Utilisateur supprimé')
+                        ->body('L\'utilisateur a été supprimé avec succès.')
+                        ->success()
+                        ->send();
+
+                    return redirect()->route('users.index');
+                })
+                ->tooltip('Supprimer le compte')
+                ->color('danger')
+                ->modalHeading('Supprimer le compte')
+                ->modalDescription('Etes-vous sûr de vouloir supprimer ce compte ?')
+                ->hidden(function (User $user) {
+                    return $user->id == auth()->user()->id;
+                })
+                ->requiresConfirmation(),
+        ]);
     }
 
-    public function confirmUserLocking($userId)
-    {
-        $this->userIdBeingLocked = $userId;
-        $this->confirmingUserLocking = true;
-    }
-
-    public function confirmAccessCardReset($userId)
-    {
-        $this->userIdAccessCardBeingReset = $userId;
-        $this->confirmingUserAccessCardReset = true;
-    }
-
-    public function confirmUserDeleting($userId)
-    {
-        $this->userIdBeingDeletion = $userId;
-        $this->confirmingUserDeletion = true;
-    }
-
-    public function confirmUserLunch($userId)
-    {
-        $this->userIdBeingLunch = $userId;
-        $this->confirmingUserLunch = true;
-    }
 
     public function restoreCurrentCard()
     {
@@ -241,9 +221,6 @@ class UserTable extends Component implements HasTable, HasForms
             'quota_breakfast' => 0,
         ]);
 
-        $this->confirmingUserAccessCardReset = false;
-
-        $this->userIdAccessCardBeingReset = null;
 
         flasher('success', "La carte courante de l'utilisateur a été restauré!");
 
@@ -265,11 +242,6 @@ class UserTable extends Component implements HasTable, HasForms
         return redirect()->route('users.index');
     }
 
-    public function confirmUserReset($userId)
-    {
-        $this->userIdBeingReset = $userId;
-        $this->confirmingUserReset = true;
-    }
 
     public function confirmPasswordReset()
     {
@@ -293,41 +265,30 @@ class UserTable extends Component implements HasTable, HasForms
         return redirect()->route('users.index');
     }
 
-    public function lockUser()
+    public function lockUser(User $user) : void
     {
-        $user = User::find($this->userIdBeingLocked);
-
         $user->update(['is_active' => false]);
+
         ActivityHelper::createActivity(
             $user,
             "Désactivation du compte de $user->full_name",
             'Mise à jour de compte',
         );
+
         UserLocked::dispatch($user);
-
-        $this->confirmingUserLocking = false;
-        $this->userIdBeingLocked = null;
-
-        flasher('success', "L'utilisateur a été désactivé avec succès !");
-
-        return redirect()->route('users.index');
     }
 
 
-
-
-    public function deleteUser()
+    public function deleteUser(User $user) : void
     {
-        $user = User::find($this->userIdBeingDeletion);
-
-        //Annulation des commandes de l"utilisateur a supprimer
 
         $user->orders->filter(function ($order) {
-            return $order->state->title() === "Commande effectuée";
+            return $order->state instanceof \App\States\Order\Confirmed;
         })->each(fn ($order) => $order->update(['state' => Cancelled::class]));
 
 
         $identifier = Str::random(60);
+
         $user->update([
             'identifier' => $identifier,
             'email' => $identifier . '@' . $identifier . '.com',
@@ -340,25 +301,10 @@ class UserTable extends Component implements HasTable, HasForms
         );
 
         $user->delete();
-
-        $this->confirmingUserDeletion = false;
-
-        $this->userIdBeingDeletion = null;
-
-        session()->flash('success', "L'utilisateur a été supprimé avec succès !");
-
-        return redirect()->route('users.index');
     }
 
-    public function confirmUserUnlocking($userId)
+    public function unlockUser(User $user) : void
     {
-        $this->userIdBeingUnlocked = $userId;
-        $this->confirmingUserUnlocking = true;
-    }
-
-    public function unlockUser()
-    {
-        $user = User::find($this->userIdBeingUnlocked);
 
         $user->update(['is_active' => true]);
 
@@ -369,14 +315,6 @@ class UserTable extends Component implements HasTable, HasForms
         );
 
         UserUnlocked::dispatch($user);
-
-        $this->confirmingUserUnlocking = false;
-
-        $this->userIdBeingUnlocked = null;
-
-        session()->flash('success', "L'utilisateur a été activé avec succès !");
-
-        return redirect()->route('users.index');
     }
 
     public function exportQuota()
@@ -388,12 +326,6 @@ class UserTable extends Component implements HasTable, HasForms
     public function exportToUser()
     {
         return Excel::download(new UserExport(), 'utilisateurs.xlsx');
-    }
-
-
-    public function modalsView(): string
-    {
-        return 'livewire.users.modals';
     }
 
     public function render()

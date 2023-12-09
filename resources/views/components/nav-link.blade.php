d1d5e859ef2a18a3853af9297bd6665389ea7884
roomdada <dsieroger@gmail.com>
2023-12-09 23:24:44 +0100

    wip

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,6 @@
 @php
 $classes = $active ?? false ? 'flex items-center space-x-3 px-3 font-medium rounded text-white bg-primary-900' : 'flex items-center space-x-3 px-3 font-medium rounded text-gray-50 hover:text-gray-700 hover:bg-gray-50 active:bg-primary-50';
 @endphp
-
 <a {{ $attributes->merge(['class' => $classes]) }}>
     <span class="flex-none flex items-center hover:text-primary-800 opacity-50">
         <x-icon name="{{ $icon }}" />

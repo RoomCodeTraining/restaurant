f6aff888346cb45402a3f0224e49f42a963ace27
roomdada <dsieroger@gmail.com>
2023-12-19 09:02:29 +0100

    wip

---
+++ app/Http/Livewire/Orders/CreateSpecialOrderForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Orders;
+
+use Livewire\Component;
+
+class CreateSpecialOrderForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.orders.create-special-order-form');
+    }
+}

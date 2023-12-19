e0688e0686ec771dd4fb97d73fa789c1a1783075
Dydja <kadidjabayoko@gmail.com>
2023-12-19 17:14:37 +0100

    Update activate button

---
+++ .history/app/Http/Livewire/Tables/BreakfastCompletedTable_20231219170254.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Livewire\Tables;
+
+use App\Models\Order;
+use App\States\Order\Completed;
+use Carbon\Carbon;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\Indicator;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+
+class BreakfastCompletedTable extends Component implements HasTable, HasForms
+{
+    use InteractsWithTable, InteractsWithForms;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                \App\Models\Order::query()
+                    ->whereState('state', Completed::class)
+                    ->whereIn('type', ['lunch', 'breakfast'])
+                    ->whereUserId(Auth::id())
+                    ->withoutGlobalScope('lunch'),
+            )
+            ->columns([
+                //TextColumn::make('id')->label('Pointage du')->formatStateUsing(fn (Order $row) => $row->type == 'lunch' ? $row->menu->served_at->format('d/m/Y') : $row->created_at->format('d/m/Y')),
+                TextColumn::make('pointed_at')->label('Pointage du')->formatStateUsing(fn (Order $row) => $row->pointed_at->format('d/m/Y H:i:s')),
+                TextColumn::make('type')->label('Type')->formatStateUsing(fn (Order $row) => $row->type == 'lunch' ? 'Déjeuner' : 'Petit déjeuner'),
+            ])
+            ->filters([
+                Filter::make('created_at')
+                    ->form([
+                        DatePicker::make('Du'),
+                        DatePicker::make('Au'),
+                    ])
+                    // ...
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+
+                        if ($data['Du'] ?? null) {
+                            $indicators[] = Indicator::make('Du ' . Carbon::parse($data['Du'])->toFormattedDateString())
+                                ->removeField('Du');
+                        }
+
+                        if ($data['Au'] ?? null) {
+                            $indicators[] = Indicator::make('Jusqu\'au ' . Carbon::parse($data['Au'])->toFormattedDateString())
+                                ->removeField('Au');
+                        }
+
+                        return $indicators;
+                    })
+
+            ])->headerActions([
+                ExportAction::make()->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename(date('d-m-Y') . '- HistoriquesPointages - export'),
+                ]),
+            ])
+            ->emptyStateHeading('Aucun historique de pointage disponible pour le moment');
+    }
+
+    public function render()
+    {
+        return view('livewire.tables.breakfast-completed-table');
+    }
+}
---
+++ .history/app/Http/Livewire/Tables/BreakfastReportingTable_20231219171215.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Livewire\Tables;
+
+use App\Models\Order;
+use App\States\Order\Completed;
+use App\Support\DateTimeHelper;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Component;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+
+class BreakfastReportingTable extends Component implements HasTable, HasForms
+{
+    use InteractsWithTable, InteractsWithForms;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(self::getTableQuery())
+            ->columns([
+                // TextColumn::make('id')
+                //     ->formatStateUsing(fn (Order $row) => $row->created_at->format('d/m/Y'))
+                //     ->label('POINTAGE DU')
+                //     ->searchable()
+                //     ->sortable(),
+                TextColumn::make('pointing_at')
+                    ->formatStateUsing(fn (Order $row) => $row->created_at->format('d/m/Y'))
+                    ->label('POINTAGE DU')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('user.identifier')->label('MATRICULE/IDENTIFIANT'),
+                TextColumn::make('user.full_name')->label('NOM & PRÉNOM')->searchable(),
+                // TextColumn::make('state')
+                //     ->label('Utilisateur')
+                //     ->formatStateUsing(fn (Order $row) => $row->user->full_name),
+            ])
+            ->headerActions([
+                ExportAction::make()->exports([
+                    ExcelExport::make()
+                        ->fromTable()
+                        ->withFilename(date('d-m-Y') . '- breakfastReporting - export'),
+                ]),
+            ])
+            ->filters([
+                Filter::make('created_at')
+                    ->form([
+                        Select::make('period')
+                            ->options(DateTimeHelper::getPeriod())
+                            ->default('this_week')
+                            ->label('Période'),
+                    ])
+                    ->query(function (Builder $query, array $data) {
+                        return $query->when($data['period'], fn ($query, $period) => $query->whereBetween('created_at', DateTimeHelper::inThePeriod($period)));
+                    }),
+            ])
+            ->emptyStateHeading('Aucun petit déjeuner trouvé')
+            ->emptyStateIcon('heroicon-o-sun');
+    }
+
+    private static function getTableQuery()
+    {
+        return \App\Models\Order::query()
+            ->whereState('state', Completed::class)
+            ->whereIn('type', ['breakfast'])
+            ->with('user')
+            ->withoutGlobalScope('lunch')
+            ->latest();
+    }
+
+    public function render()
+    {
+        return view('livewire.tables.weekly-order-table');
+    }
+}

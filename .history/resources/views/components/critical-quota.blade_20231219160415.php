2373053d639410130cae0fd90249843d44f584a3
Dydja <kadidjabayoko@gmail.com>
2023-12-20 13:27:59 +0100

    Update color for quota

---
+++ .history/resources/views/components/critical-quota.blade_20231219160415.php
@@ -0,0 +1,25 @@
+    <?php
+    $card = auth()->user()->accessCard;
+    $quota_lunch = $card?->quota_lunch;
+    $quota_breakfast = $card?->quota_breakfast;
+    ?>
+    @if($card && $card->hasCriticalQuota())
+    <div class='py-8 px-4'>
+        <div class="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+            role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor" viewBox="0 0 20 20">
+                <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>
+                <span class="font-medium">Quota critique!</span><br>
+                @if($quota_lunch <= config('cantine.quota_critical'))
+                    <span class="text-sm">Votre allocation pour le déjeuner est presque épuisée. Merci de penser à la recharger.</span>
+                 @elseif($quota_breakfast <= config('cantine.quota_critical'))
+                    <span class="text-sm">Votre allocation pour le petit déjeuner est presque épuisée. Merci de penser à la recharger.</span>
+                @endif
+            </div>
+        </div>
+    @endif

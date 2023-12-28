67ab3fbfc3400708821637601ac18a5475002ad6
roomdada <dsieroger@gmail.com>
2023-12-28 20:16:36 +0100

    wip

--- app/Actions/AccessCard/CreatePrimaryCardAction.php
+++ app/Actions/AccessCard/CreatePrimaryCardAction.php
@@ -7,35 +7,36 @@ use App\Models\User;
 
 class CreatePrimaryCardAction
 {
-  public function handle(User $user, array $input): AccessCard
-  {
-    // $hasCurrentAccessCard = $user->currentAccessCard;
+    public function handle(User $user, array $input): AccessCard
+    {
+        // $hasCurrentAccessCard = $user->currentAccessCard;
 
-    $accessCard = $user->accessCards()->create([
-      'identifier' => $input['identifier'],
-      'quota_breakfast' => $user->currentAccessCard->quota_breakfast ?? $input['quota_breakfast'],
-      'quota_lunch' => $user->currentAccessCard->quota_lunch ?? $input['quota_lunch'],
-      'payment_method_id' => $input['payment_method_id'] ?? $user->userType->paymentMethod->id,
-      'is_used' => true,
-      'type' => AccessCard::TYPE_PRIMARY,
-    ]);
+        $accessCard = $user->accessCards()->create([
+          'identifier' => $input['identifier'],
+          'quota_breakfast' => $user->currentAccessCard->quota_breakfast ?? $input['quota_breakfast'],
+          'quota_lunch' => $user->currentAccessCard->quota_lunch ?? $input['quota_lunch'],
+          'payment_method_id' => $input['payment_method_id'] ?? $user->userType?->paymentMethod?->id,
+          'is_used' => true,
+          'type' => AccessCard::TYPE_PRIMARY,
+        ]);
 
-    $this->accessCardHasNewQuota($accessCard);
-    return $accessCard;
-  }
+        $this->accessCardHasNewQuota($accessCard);
 
-
-  public function accessCardHasNewQuota($accessCard)
-  {
-
-    if ($accessCard->quota_breakfast != 0) {
-      $accessCard->createReloadHistory('breakfast');
+        return $accessCard;
     }
 
-    if ($accessCard->quota_lunch != 0) {
-      $accessCard->createReloadHistory('lunch');
-    }
 
-    return $accessCard;
-  }
+    public function accessCardHasNewQuota($accessCard)
+    {
+
+        if ($accessCard->quota_breakfast != 0) {
+            $accessCard->createReloadHistory('breakfast');
+        }
+
+        if ($accessCard->quota_lunch != 0) {
+            $accessCard->createReloadHistory('lunch');
+        }
+
+        return $accessCard;
+    }
 }
